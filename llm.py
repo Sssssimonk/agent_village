13bd1bf216e96f87362f0c84750a29f43a4069bd
Simon Chen <simon.chensiyu@hotmail.com>
2024-01-26 16:58:39 -0800

    Create llm.py

---
+++ llm.py
@@ -0,0 +1,35 @@
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import json
+import warnings
+import torch
+import BitsAndBytesConfig
+
+
+bnb_config = BitsAndBytesConfig(load_in_4bit=True,
+                                bnb_4bit_use_double_quant=True,
+                                bnb_4bit_quant_type="nf4",
+                                bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+# use llama2 model in transfomers
+access_token = "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"
+model_id = "meta-llama/Llama-2-7b-chat-hf"
+tokenizer = AutoTokenizer.from_pretrained(model_id, 
+                                          token=access_token)
+model = AutoModelForCausalLM.from_pretrained(model_id,
+                                             quantization_config=bnb_config,
+                                             token=access_token,
+                                             device_map="auto")
+
+
+pipe = pipeline(task="text-generation", 
+                model=model, 
+                tokenizer=tokenizer
+                #PretrainedConfig = xxx
+                )
+print("hf_model_initialized")
+
+
+def generate_prompt(task):
+    # from prompt file task.txt, read the prompt template and then out put a str prompt.
