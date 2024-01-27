cf52d6ba16ad0ea3a7e0d6967fbf3915abf8a546
Simon Chen <simon.chensiyu@hotmail.com>
2024-01-26 16:42:05 -0800

    add main

---
+++ main.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    while True:
+        count = input("Enter iteration to run: ")
+        while count > 0:
+            run_simulation()
+            count -= 1
+            
